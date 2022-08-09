bfeb819523c2f1ad78ccb0a04c46d023ac550bfe
naufal-fa <ardmisters@gmail.com>
2022-08-09 14:11:49 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import ProductAdd from "./components/Product/ProductAdd.js";
+import ProductEdit from "./components/Product/ProductEdit.js";
+import ProductsList from "./components/Product/ProductsList.js";
+import UserAdd from "./components/User/UserAdd.js";
+import UserEdit from "./components/User/UserEdit.js";
+import UserList from "./components/User/UserList.js";
+import "./css/main.css";
+import "./js/main.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css"></link>
+      <link rel="dns-prefetch" href="https://fonts.gstatic.com"></link>
+      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"></link>
+      <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/dashboard" element={<Dashboard/>}/>
+        <Route exact path="/product" element={<ProductsList/>}/>
+        <Route exact path="/product/add" element={<ProductAdd/>}/>
+        <Route exact path="/product/edit/:id" element={<ProductEdit />}/>
+        <Route exact path="/user" element={<UserList/>}/>
+        <Route exact path="/user/add" element={<UserAdd/>}/>
+        <Route exact path="/user/edit/:id" element={<UserEdit />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
